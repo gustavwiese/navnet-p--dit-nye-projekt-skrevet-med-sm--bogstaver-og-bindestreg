058c19e7935230aff26588146ab5d34c3dd7dad4
gustavwiese <gusse12345@gmail.com>
2023-01-31 13:20:31 +0100

    Ændringer

---
+++ app.js
@@ -0,0 +1,5 @@
+document.body.innerHTML += "Hello JavaScript!";
+document.body.innerHTML += "Hello JavaScript!";
+//alert("Hello Dat.js");
+console.log("Hello Dat.js");
+console.log("Hej Filippa.js");
